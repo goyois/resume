545da761efef93a13e513b5b83db5d5bf56e3e9d
SeungWoo1222 <wlstmddn98@naver.com>
2026-04-15 16:47:51 +0900

    init

---
+++ src/main/java/com/toyboyz/swportfolio/SwPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.toyboyz.swportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyboyz/swportfolio/SwPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyboyz.swportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
